42cb43283cfaf23b955bc920cc116ae8b3124902
kanok <kanok.ki@hotmail.com>
2018-11-28 11:01:42 +0700

    update app

--- src/main/java/com/autobot/line/Application.java
+++ src/main/java/com/autobot/line/Application.java
@@ -1,7 +1,6 @@
 package com.autobot.line;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.linecorp.bot.model.event.MessageEvent;
