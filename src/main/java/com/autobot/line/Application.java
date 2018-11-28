f72601fd5a67c6bbd2d2a36b2e0f05bbf9125338
kanok <kanok.ki@hotmail.com>
2018-11-28 10:51:22 +0700

    edit config

--- src/main/java/com/autobot/line/Application.java
+++ src/main/java/com/autobot/line/Application.java
@@ -2,14 +2,17 @@ package com.autobot.line;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.linecorp.bot.model.event.MessageEvent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
 import com.linecorp.bot.model.message.Message;
 import com.linecorp.bot.model.message.TextMessage;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
+import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 
-@EnableAutoConfiguration
+@SpringBootApplication
+@LineMessageHandler
 public class Application {
 	public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
